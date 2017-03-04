ab7496d1b633e48ffea4dab4fa43146e3d2adc19
John Daly <jpdaly@gmail.com>
2017-03-04 07:55:27 +0000

    some refactoring of regex to stop more complex expressions breaking parser

--- app/src/main/java/com/codeclan/example/chordisto/Parser.java
+++ app/src/main/java/com/codeclan/example/chordisto/Parser.java
@@ -1,6 +1,5 @@
 package com.codeclan.example.chordisto;
 
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,17 +12,17 @@ import static com.codeclan.example.chordisto.TriadType.*;
 public class Parser {
 
     //instance variables
-    private String root;
+    private RootName root;
     private TriadType triad;
     private Pattern pattern;
 
     //constructicon
     public Parser() {
-        pattern = Pattern.compile("([A-G][#b]?)([m7])?");
+        pattern = Pattern.compile("([A-G][#b]?)([m7])?(.*)");
     }
 
     //getters
-    public String getRoot() {
+    public RootName getRoot() {
         return root;
     }
 
@@ -36,28 +35,57 @@ public class Parser {
         return string.split("[, ]+");
     }
 
-    private void setRoot(String rootInfo){
+    private void setRoot(String rootInfo) {
+        String enumFormat;
+        String tonality = null;
+        String accidentality = null;
+        Pattern rootPattern = Pattern.compile("([A-G])([b#]?)");
+        Matcher m = rootPattern.matcher(rootInfo);
+
+        if (m.matches()) {
+            tonality = m.group(1);
+            accidentality = m.group(2);
+
+        }
+
+        if (accidentality != null) {
+            switch (accidentality) {
+                case "#":
+                    accidentality = "SHARP";
+                    break;
+                case "b":
+                    accidentality = "FLAT";
+                    break;
+            }
+            enumFormat = tonality + accidentality;
+        } else {
+            enumFormat = tonality;
+        }
+
+        root = RootName.valueOf(enumFormat);
 
     }
 
     private void setTriad(String triadInfo) {
-        if (triadInfo == null){
+        if (triadInfo == null) {
             triad = MAJOR;
         } else if (triadInfo.equals("m")) {
             triad = MINOR;
-        } else if (triadInfo.equals("7")){
+        } else if (triadInfo.equals("7")) {
             triad = DOMINANT;
         }
     }
 
-    
+
     public void setVariables(String chord) {
         String triadType = null;
         String rootName = null;
+        String theRest = null;
         Matcher m = pattern.matcher(chord);
         if (m.matches()) {
             rootName = m.group(1);
             triadType = m.group(2);
+//            theRest = m.group(3);
         }
 
         setRoot(rootName);
--- app/src/test/java/com/codeclan/example/chordisto/TestParser.java
+++ app/src/test/java/com/codeclan/example/chordisto/TestParser.java
@@ -3,7 +3,7 @@ package com.codeclan.example.chordisto;
 import org.junit.Before;
 import org.junit.Test;
 
-import static com.codeclan.example.chordisto.RootNames.*;
+import static com.codeclan.example.chordisto.RootName.*;
 import static com.codeclan.example.chordisto.TriadType.*;
 import static junit.framework.Assert.assertEquals;
 
@@ -28,101 +28,60 @@ public class TestParser {
     }
 
     @Test
-    public void testRegexGetsSimpleRoot(){
+    public void testRegexGetsSimpleRoot() {
         String chord = "C";
         parser.setVariables(chord);
         assertEquals(C, parser.getRoot());
     }
 
-//    @Test
-//    public void testRegexGetsRootWithAccidental(){
-//        String chord = "Bb";
-//        parser.setVariables(chord);
-//        assertEquals(BFLAT, parser.getRoot());
-//
-//        String otherChord = "F#";
-//        parser.setVariables(otherChord);
-//        assertEquals(FSHARP, parser.getRoot());
-//    }
-//
-//    @Test
-//    public void testGetRootFromLongerSymbol(){
-//        String chord = "Gm";
-//        parser.setVariables(chord);
-//        assertEquals(G, parser.getRoot());
-//    }
+    @Test
+    public void testRegexGetsRootWithAccidental() {
+        String chord = "Bb";
+        parser.setVariables(chord);
+        assertEquals(BFLAT, parser.getRoot());
+    }
 
     @Test
-    public void testSetTriadMAJOREdition(){
+    public void testRootCanBeSharp() {
+        String otherChord = "F#";
+        parser.setVariables(otherChord);
+        assertEquals(FSHARP, parser.getRoot());
+    }
+
+
+    @Test
+    public void testGetRootFromLongerSymbol() {
+        String chord = "Gm";
+        parser.setVariables(chord);
+        assertEquals(G, parser.getRoot());
+    }
+
+    @Test
+    public void testSetTriadMAJOREdition() {
         String chord = "G";
         parser.setVariables(chord);
         assertEquals(MAJOR, parser.getTriad());
     }
 
     @Test
-    public void testSetTriadMinorEdition(){
+    public void testSetTriadMinorEdition() {
         String chord = "Gm";
         parser.setVariables(chord);
         assertEquals(MINOR, parser.getTriad());
     }
 
     @Test
-    public void testSetDominantTriad(){
+    public void testSetDominantTriad() {
         String chord = "F#7";
         parser.setVariables(chord);
         assertEquals(DOMINANT, parser.getTriad());
     }
 
-
-//    @Test
-//    public void testCanBreakSymbolIntoChunks() {
-//        String chord = "F7";
-//        assertEquals(2, parser.getElements().size());
-//
-//    }
-//
-//    @Test
-//    public void testCanDeleteRootNoteFromSimpleChord(){
-//        String chord = "Bb7";
-//        parser.splitChordSymbol(chord);
-//        parser.deleteElement();
-//        assertEquals(2, parser.getElements().size());
-//    }
-//
-//    @Test
-//    public void testUseRegexToPullRootNote() {
-//        String chord = "F7b9";
-//        parser.splitChordSymbol(chord);
-//        parser.setRoot();
-//        assertEquals("F", parser.getRoot());
-//    }
-//
-//    @Test
-//    public void testUseRegexToPullAccidentalRootNote(){
-//        String chord = "F#7b9";
-//        parser.splitChordSymbol(chord);
-//        parser.setRoot();
-//        assertEquals("F#", parser.getRoot());
-//    }
-//
-//    @Test
-//    public void testGetRootNoteFromLudicrouslyComplexChordSymbol(){
-//        String chord = "Bb7b11b13";
-//        parser.splitChordSymbol(chord);
-//        assertEquals("Bb", parser.getRoot());
-//    }
-//
-//    @Test
-//    public void testGetChordTypeMajorNonAccidental(){
-//        String chord = "C";
-//        assertEquals(MAJOR, parser.getTriad());
-//    }
-//
-//    @Test
-//    public void testRecognisesMinorChords(){
-//        String chord = "Cm";
-//        parser.setRoot();
-//        parser.setTriad();
-//        assertEquals(MINOR, parser.getTriad());
-//    }
+    @Test
+    public void testDoesNotSpitDummyWhenMoreInfoGivenThanExpected(){
+        String chord = "Db7#9b11";
+        parser.setVariables(chord);
+        assertEquals(DFLAT, parser.getRoot());
+        assertEquals(DOMINANT, parser.getTriad());
+    }
 }
