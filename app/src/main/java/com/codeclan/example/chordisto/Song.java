a595d3b9bc9ca3a4f6e95e478900aba036c766f8
John Daly <jpdaly@gmail.com>
2017-03-06 15:30:15 +0000

    add variables, constructor and getters to song class

--- app/src/main/java/com/codeclan/example/chordisto/Song.java
+++ app/src/main/java/com/codeclan/example/chordisto/Song.java
@@ -5,4 +5,20 @@ package com.codeclan.example.chordisto;
  */
 
 public class Song {
+
+    String title;
+    String chords;
+
+    public Song(String title, String chords){
+        this.title = title;
+        this.chords = chords;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getChords() {
+        return chords;
+    }
 }
