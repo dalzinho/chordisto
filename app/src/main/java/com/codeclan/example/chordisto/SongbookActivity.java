78aaae2968158ab60c996c40e800da5554c96002
John Daly <jpdaly@gmail.com>
2017-03-07 14:07:25 +0000

    begin to create list view

--- app/src/main/java/com/codeclan/example/chordisto/SongbookActivity.java
+++ app/src/main/java/com/codeclan/example/chordisto/SongbookActivity.java
@@ -1,13 +1,89 @@
 package com.codeclan.example.chordisto;
 
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class SongbookActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songbook);
+
+    listView = (ListView)findViewById(R.id.songbook_list_view);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        List<Song> songList = db.getAllSongs();
+        ListAdapter<Song> listAdapter = new ListAdapter() {
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled(int i) {
+                return false;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int i) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        }
     }
 }
