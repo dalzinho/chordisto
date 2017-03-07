19eaa86f07e52d1e7da5898be7d19985114306db
John Daly <jpdaly@gmail.com>
2017-03-07 13:38:03 +0000

    add overloaded static run methods for songbuilder

--- app/src/main/java/com/codeclan/example/chordisto/SongBuilder.java
+++ app/src/main/java/com/codeclan/example/chordisto/SongBuilder.java
@@ -5,4 +5,26 @@ package com.codeclan.example.chordisto;
  */
 
 public class SongBuilder {
+
+    private static int id;
+    private static String title;
+    private static String chords;
+    private int tempo;
+
+    public SongBuilder(){
+        title = "Untitled Song";
+        chords = "";
+        tempo = 100;
+    }
+
+    public static Song run(String inputTitle, String inputChords, int inputTempo){
+
+        return new Song(inputTitle, inputChords, inputTempo);
+    }
+
+    public static Song run(int id, String inputTitle, String inputChords, int inputTempo){
+
+        return new Song(id, inputTitle, inputChords, inputTempo);
+        
+    }
 }
